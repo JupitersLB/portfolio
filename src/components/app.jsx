08285db48e0001e7741418ca8c48b3c576e46c3d
Liam Baker <liam.baker2310@gmail.com>
2020-09-06 19:00:47 +0900

    improving navbar and layout

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -10,15 +10,16 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
-
-import About from './about';
 import Projects from './projects';
 import Current from './current';
+import GitHubBar from './gitHubBar';
+import Home from './home';
 
 export default class App extends Component {
   render() {
     return (
       <Router>
+        <GitHubBar />
         <div className="my-navbar">
           <div className="my-avatar"></div>
           <div className="my-name">
@@ -27,29 +28,35 @@ export default class App extends Component {
           <div className="my-title">
             <h3>Full Stack Developer</h3>
           </div>
-          <div className="icons">
-            <a href="https://www.linkedin.com/in/jupiters-liam-baker/" target="blank"><FontAwesomeIcon icon={faLinkedin} /></a>
-            <a href="https://github.com/JupitersLB" target="blank"><FontAwesomeIcon icon={faGithub} /></a>
-            <a href="#"><FontAwesomeIcon icon={faEnvelope} /></a>
+          <div className="about-me">
+            <p>
+              I'm a full stack developer who builds off inspiration from previous
+              experiencs.  After attending <a href="https://www.switchup.org/rankings/best-coding-bootcamps" target="blank"> the best coding bootcamp in the world</a>,
+              I have focused on using the skills I gained during the bootcamp and making sure I go
+              further to learn React, Redux, and Python.
+            </p>
           </div>
           <div className="profile-links text-center">
-            <Link to="/about"><h5>About Me</h5></Link>
             <Link to="/projects"><h5>Projects</h5></Link>
             <Link to="/current"><h5>Current</h5></Link>
           </div>
           <div className="my-footer">
-            <p>This will be an amazing footer!</p>
+            <div className="icons">
+              <a href="https://www.linkedin.com/in/jupiters-liam-baker/" target="blank"><FontAwesomeIcon icon={faLinkedin} /></a>
+              <a href="https://github.com/JupitersLB" target="blank"><FontAwesomeIcon icon={faGithub} /></a>
+              <a href="#"><FontAwesomeIcon icon={faEnvelope} /></a>
+            </div>
           </div>
         </div>
 
         <Switch>
-          <Route exact path="/about">
-            <About />
+          <Route path="/">
+            <Home />
           </Route>
-           <Route path="/projects">
+          <Route path="/projects">
             <Projects />
           </Route>
-           <Route path="/current">
+          <Route path="/current">
             <Current />
           </Route>
         </Switch>
