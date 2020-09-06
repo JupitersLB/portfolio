1ac9c79026d81cf99101cb40fe22d095bbd6030e
Liam Baker <liam.baker2310@gmail.com>
2020-09-07 00:46:07 +0900

    access to daily commit counts

---
+++ src/components/gitHubScrape.js
@@ -0,0 +1,41 @@
+const fetch = require("node-fetch");
+
+const repoListUrl = 'https://api.github.com/users/JupitersLB/repos';
+
+const baseUrl = 'https://api.github.com/';
+
+const headers = { 'Authorization' : 'Token 572e141219075fc960bc8d3cd780784621b692e6' };
+
+const repoCounts = (repoName) => {
+  const url = `${baseUrl}repos/JupitersLB/${repoName}/stats/punch_card`;
+  console.log(url);
+  fetch(url, {
+    'headers': headers
+  })
+    .then(r => r.json())
+    .then(d => console.log(d));
+};
+
+// const dailyCount = `${baseUrl}repos/JupitersLB/${repoName}/stats/punch_card`;
+
+// const api = () => {
+//   fetch('https://api.github.com/users/JupitersLB/repos')
+//     .then(r => r.json())
+//     .then(repos => repos.map((repoName) => {
+//       fetch(`https://api.github.com/repos/JupitersLB/${repoName}/stats/punch_card`)
+//         .then(r => r.json())
+//         .then(data => console.log(data));
+//     }));
+// };
+
+const api = () => {
+  fetch(repoListUrl, {
+    'headers': headers
+  })
+    .then(r => r.json())
+    .then(repos => repos.map((r) => {
+      repoCounts(r.name);
+    }));
+};
+
+api();
