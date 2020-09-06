cff3adc60b6f1fe8711039318c1766afcacb200e
Liam Baker <liam.baker2310@gmail.com>
2020-09-06 15:12:59 +0900

    components started

---
+++ src/components/app.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+import NavBar from './navBar';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <h1>Hello</h1>
+      </div>
+    );
+  }
+}
---
+++ src/components/gitHubBar.jsx
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+export class GitHubBar extends Component {
+  render() {
+    return (
+      <div></div>
+    );
+  }
+}
