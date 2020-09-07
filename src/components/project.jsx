a49ccd2b2c4b5291409c62f9cb3273922bb69da1
Liam Baker <liam.baker2310@gmail.com>
2020-09-08 00:57:21 +0900

    improved layout of cards

--- src/components/project.jsx
+++ src/components/project.jsx
@@ -4,7 +4,7 @@ export default class Project extends Component {
   render() {
     const project = this.props;
     return (
-      <div className="project-card" style={{backgroundImage: `url(${project.data.imageUrl}`}}>
+      <div className="project-card" style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.2)), url('${project.data.imageUrl}')` }}>
         <h1>{project.data.name}</h1>
       </div>
     );
