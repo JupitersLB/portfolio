2d92c168bb1c3901281dfc9eeec0d3b522ee475c
Liam Baker <liam.baker2310@gmail.com>
2020-09-09 00:19:22 +0900

    added target, pain, solution to projects

--- data/projects.js
+++ data/projects.js
@@ -3,6 +3,9 @@ const projects = [
     id: 1,
     name: 'Yours',
     role: 'full stack developer',
+    target: 'Schools and educational institutions which want to bring their students into a the digital world with guidance and support.',
+    pain: 'With kids becoming digitally native at a much younger age, learning how to navigate and interact with social media is increasingly more important. Bullying, fake news, and scams have massively negative consequences to kids who struggle with dealing with a 24/7 form of constant feedback and interaction with society.',
+    solution: 'This app is a social media which will only be accessible to students of the same school. By creating a social media intranet, students will interactive with classmates and peers is a safe space, and be introduced on a much smaller scale to how social media work and the issues which are associated with it. Their blogs, comments, and profiles are only accessible to mutal friends. They also have the opportunity for direct messaging and communicating with friends privately.',
     url: 'https://journal-yours.herokuapp.com',
     description: 'An onboarding social media app educating students in schools for the 21st Century education.',
     imageUrl: '/assets/images/avatar.png'
@@ -11,14 +14,20 @@ const projects = [
     id: 2,
     name: 'Medi+Safe',
     role: 'front-end developer',
+    target: 'The idea behind this project was to help support nursing staff in hospitals, specifically the lead and member nurses.',
+    pain: 'As a group project, one of the teammates explained the issues that can occur by poor communication or issues in organizing tasks. Lead nurses have to spend extra time ensuring nurse work loads correspond to abiltiies while serving patients\' needs. They also need to follow up on nurses progress and help or rearrange as necessary. Member nurses spend a lot of time figuring out their schedules with patients and collating information from several different sources. At the same time, they need be aware of their own progress, communicate with other staff, and beaware of any updates.',
+    solution: 'By assessing the problems that nursing staff go through, we knew that we wanted to automate patient assignment based on important criteria. After patients were assigned, we wanted to make sure that the Lead nurse could easily track their teams progress. The member nurses wanted a quick overview of their day including the number of tasks, their patients\' severity levels, and a list of their patients. They wanted extra information provided to them for each patient and a way to organize all of their tasks and optimize their schedules. By using a drag and drop system, it easily allowed customization of schedules which were persistent. A notification system allowed for updates to be received along with an assistance buton which directly communicates to their lead nurse.',
     url: 'https://www.medi-safe.me',
-    description: 'An app targeting medical staff working at hospitals with communication and task management issues.',
+    description: 'An app which helps solve task management and communicaton issues within hospitals.',
     imageUrl: '/assets/images/medi-safe.png'
   },
   {
     id: 3,
     name: 'Mister Cocktail',
     role: 'full stack developer',
+    target: 'People who enjoy cocktails with an exciting UI.',
+    pain: 'Keeping track of cocktails and doses can be difficult.',
+    solution: 'A cool, interactive way to record cocktails and doses and will give you suggestions of other cocktails you might like.',
     url: 'https://jupiters-balltails.herokuapp.com',
     description: 'Cocktail app primarily exploring MVC framework, built with Ruby on Rails and JavaScript packages.',
     imageUrl: '/assets/images/mister-cocktail.png'
@@ -27,6 +36,9 @@ const projects = [
     id: 4,
     name: 'Farm-Me',
     role: 'back-end developer',
+    target: 'Farmers and members of the local community who enjoy fresh vegetables, care about sustainabilty, and supporting the local economy.',
+    pain: 'Farmers and consumers often interact with 3rd parties during production and counsumption which can raise costs and decrease profits. They also might need become dependent and susceptible to current global politics.',
+    solution: 'The app allows farmers to post their products in the form of a weekly basket of produce which users can subscribe too. Both parties get to decide whether to enter into the trade or stop at any point based on distances and quality.',
     url: 'http://farm-me.herokuapp.com',
     description: 'A great marketplace for farmers to connect with and sell to their local communities taking distance into account.',
     imageUrl: '/assets/images/farm-me.png'
@@ -35,6 +47,9 @@ const projects = [
     id: 5,
     name: 'Freelance',
     role: 'full stack developer',
+    target: '',
+    pain: '',
+    solution: '',
     url: 'https://www.growing-trees.com/LTonline/',
     description: 'Maintaining and developing and content and information develiery system.',
     imageUrl: '/assets/images/avatar.png'
--- src/components/project.jsx
+++ src/components/project.jsx
@@ -10,11 +10,23 @@ export default class Project extends Component {
           <h1 className="project-title">{project.name}</h1>
           <h1 className="project-role"><small>{project.role}</small></h1>
         </div>
+        <div className="project-description">
+          <h4>{project.description}</h4>
+        </div>
         <a href={project.url} target="blank">
           <div className="project-image" style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.1)), url('${project.imageUrl}')` }}></div>
         </a>
-        <div className="project-description">
-          <h3>{project.description}</h3>
+        <div className="project-target">
+          <h3>Target</h3>
+          <p>{project.target}</p>
+        </div>
+        <div className="project-pain">
+          <h3>Pain</h3>
+          <p>{project.pain}</p>
+        </div>
+        <div className="project-solution">
+          <h3>Solution</h3>
+          <p>{project.solution}</p>
         </div>
       </div>
     );
