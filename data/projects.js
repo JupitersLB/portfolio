29147692194235c4079849425a7b0a872d3ca964
Liam Baker <liam.baker2310@gmail.com>
2020-09-08 22:54:25 +0900

    added new project and fixed scroll height

--- data/projects.js
+++ data/projects.js
@@ -1,32 +1,44 @@
 const projects = [
   {
     id: 1,
-    name: 'Mister Cocktail',
-    url: 'https://jupiters-balltails.herokuapp.com',
-    description: 'Cocktail app primarily exploring MVC framework, built with Ruby on Rails and JavaScript packages.',
-    imageUrl: '/assets/images/mister-cocktail.png'
+    name: 'Yours',
+    role: 'full stack developer',
+    url: 'https://journal-yours.herokuapp.com',
+    description: 'An onboarding social media app educating students in schools for the 21st Century education.',
+    imageUrl: '/assets/images/avatar.png'
   },
   {
     id: 2,
-    name: 'Farm-Me',
-    url: 'http://farm-me.herokuapp.com',
-    description: 'A great marketplace for farmers to connect with and sell to their local communities taking distance into account.',
-    imageUrl: '/assets/images/farm-me.png'
-  },
-  {
-    id: 3,
     name: 'Medi+Safe',
+    role: 'front-end developer',
     url: 'https://www.medi-safe.me',
     description: 'An app targeting medical staff working at hospitals with communication and task management issues.',
     imageUrl: '/assets/images/medi-safe.png'
   },
   {
+    id: 3,
+    name: 'Mister Cocktail',
+    role: 'full stack developer',
+    url: 'https://jupiters-balltails.herokuapp.com',
+    description: 'Cocktail app primarily exploring MVC framework, built with Ruby on Rails and JavaScript packages.',
+    imageUrl: '/assets/images/mister-cocktail.png'
+  },
+  {
     id: 4,
-    name: 'Yours',
-    url: 'https://journal-yours.herokuapp.com',
-    description: 'An onboarding social media app educating students in schools for the 21st Century education.',
+    name: 'Farm-Me',
+    role: 'back-end developer',
+    url: 'http://farm-me.herokuapp.com',
+    description: 'A great marketplace for farmers to connect with and sell to their local communities taking distance into account.',
+    imageUrl: '/assets/images/farm-me.png'
+  },
+  {
+    id: 5,
+    name: 'Freelance',
+    role: 'full stack developer',
+    url: 'https://www.growing-trees.com/LTonline/',
+    description: 'Maintaining and developing and content and information develiery system.',
     imageUrl: '/assets/images/avatar.png'
-  }
+  },
 ];
 
 export default projects;
--- src/components/project.jsx
+++ src/components/project.jsx
@@ -6,11 +6,16 @@ export default class Project extends Component {
     const { project } = this.props;
     return (
       <div className="project-show">
-        <h1>{project.name}</h1>
+        <div className="project-heading">
+          <h1 className="project-title">{project.name}</h1>
+          <h1 className="project-role"><small>{project.role}</small></h1>
+        </div>
         <a href={project.url} target="blank">
           <div className="project-image" style={{ backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.1)), url('${project.imageUrl}')` }}></div>
         </a>
-        <h1>{project.description}</h1>
+        <div className="project-description">
+          <h3>{project.description}</h3>
+        </div>
       </div>
     );
   }
