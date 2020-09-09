7b3918ce69a198134eef74044f70b90e2e8c4e00
Liam Baker <liam.baker2310@gmail.com>
2020-09-09 15:37:04 +0900

    access last week of commits

--- src/components/gitHubBar.jsx
+++ src/components/gitHubBar.jsx
@@ -1,7 +1,10 @@
 import React, { Component } from 'react';
 
+import counts from './gitHubScrape';
+
 export default class GitHubBar extends Component {
   render() {
+    console.log(counts.yours.promise.then(d => console.log(d.owner.reverse()[0])));
     return (
       <div className="github-banner">
         <p>This will be statistics from GitHub</p>
--- src/components/gitHubScrape.js
+++ src/components/gitHubScrape.js
@@ -1,41 +1,54 @@
-import { createRequire } from 'module';
+// import { createRequire } from 'module';
 
-import { config } from './apiToken.js';
+import { config } from './apiToken';
 
-const require = createRequire(import.meta.url);
+// const require = createRequire(import.meta.url);
 
 
-const fetch = require("node-fetch");
+// const fetch = require("node-fetch");
 
-const repoListUrl = 'https://api.github.com/users/JupitersLB/repos';
+// const repoListUrl = 'https://api.github.com/users/JupitersLB/repos';
 
 const baseUrl = 'https://api.github.com/';
 
+const mediSafeUrl = `${baseUrl}repos/JupitersLB/medi-safe/stats/participation`;
+const portfolioUrl = `${baseUrl}repos/JupitersLB/portfolio/stats/participation`;
+const yoursUrl = `${baseUrl}repos/JupitersLB/rails-journal/stats/participation`;
+const cocktailUrl = `${baseUrl}repos/JupitersLB/rails-mister-cocktail/stats/participation`;
+// const urls = [mediSafeUrl, portfolioUrl, yoursUrl, cocktailUrl];
+
 const headers = { 'Authorization' : `Token ${config.token}` };
 
-const repoCounts = (repoName) => {
-  const url = `${baseUrl}repos/JupitersLB/${repoName}/stats/punch_card`;
-  fetch(url, {
-    'headers': headers
-  })
-    .then(r => r.json())
-    .then((data) => {
-      data.reverse().map((d) => {
-        if (d[0] === 6) {
-          console.log(d[2]);
-        }
-      });
-    });
+// const repoCounts = (repoName) => {
+//   const url = `${baseUrl}repos/JupitersLB/${repoName}/stats/participation`;
+//   let count = 0;
+//   fetch(url, {
+//     'headers': headers
+//   })
+//     .then(r => r.json())
+//     .then((data) => {
+//       // const n = data[0][0];
+//       count += parseInt(data.owner.reverse()[0], 10);
+//       console.log(count);
+//     });
+// };
+
+const api = (url) => {
+  const promise = fetch(url, { 'headers': headers }).then(r => r.json());
+  return { promise };
 };
 
-const api = () => {
-  fetch(repoListUrl, {
-    'headers': headers
-  })
-    .then(r => r.json())
-    .then(repos => repos.map((r) => {
-      repoCounts(r.name);
-    }));
+
+const mediSafeCount = api(mediSafeUrl);
+const portfolioCount = api(portfolioUrl);
+const yoursCount = api(yoursUrl);
+const cocktailCount = api(cocktailUrl);
+
+const counts = {
+  mediSafe: mediSafeCount,
+  portfolio: portfolioCount,
+  yours: yoursCount,
+  cocktail: cocktailCount
 };
 
-api();
+export default counts;
